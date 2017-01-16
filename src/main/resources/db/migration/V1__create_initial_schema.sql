6467043693f555e65842f8bc3674778c5e666ad8
localadmin <raman19@gmail.com>
2017-01-16 12:03:16 +0530

    Created

---
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `movies` (
+  `Id`      INT           NOT NULL      AUTO_INCREMENT,
+  `version` INT NOT NULL  DEFAULT       0,
+  `title`   VARCHAR(45)   NOT NULL,
+  `watched` TINYINT       NULL DEFAULT  0,
+  `rating`  VARCHAR(5)    NULL,
+  `released`DATE          NULL,
+  `length`  INT           NULL,
+  `created` TIMESTAMP     NULL DEFAULT now(),
+  `modified` TIMESTAMP     NULL DEFAULT now(),
+  PRIMARY KEY (`Id`));
